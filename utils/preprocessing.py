618b1966583ca2c27369b7c2b6c2738c97f13abd
ambroisernd <ambroise.renaud@gmail.com>
2019-09-18 16:19:34 +0200

    numpy docstring for the function answer_to_one_hot()

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,6 +2,21 @@ import pandas as pd
 
 
 def answer_to_one_hot(answer, vocabulary):
+    """
+    Create a one-hot vector. indexes 0 and 1 corresponds to the result of the question, next indexes corresponds to the
+    type of the question.
+
+    Parameters
+    ----------
+    answer : Pandas DataFrame
+        a single answer (one line in the Pandas DataFrame corresponding to the dataset)
+    vocabulary : array-like
+        vocabulary (cf utils.app_utils.create_vocabulary())
+    Returns
+    -------
+    array-like
+        one-hot vector
+    """
     X = [0 for _ in range(len(vocabulary))]
     X[vocabulary.index(answer.loc["question"])] = 1
     X[vocabulary.index(answer.loc["result"])] = 1
